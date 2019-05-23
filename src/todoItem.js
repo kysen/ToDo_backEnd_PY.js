4696e630fb5d3ad88d6848b6eaea09011d3c7efc
kysen <kysenj@gmail.com>
2019-05-23 11:02:13 -0600

    bug fix in todoItem

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,45 +1,53 @@
-import React from 'react';
+import React from "react";
 
 class TodoItem extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			done: props.item.done
-		};
-	}
+    this.state = {
+      done: props.item.done
+    };
+  }
 
-	toggleDone = () => {
-		fetch(`http://localhost:5000/todo/${this.props.item.id}`, {
-			method: "PUT",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				title: this.props.item.title,
-				done: !this.state.done
-			})
-		}).then(
-			this.setState({
-				done: !this.state.done
-			})
-		)
-			.catch(error => console.log(error))
-	}
+  toggleDone = () => {
+    fetch(
+      `https://todo-list-api-python-kmj.herokuapp.com/todo/${
+        this.props.item.id
+      }`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: this.props.item.title,
+          done: !this.state.done
+        })
+      }
+    )
+      .then(
+        this.setState({
+          done: !this.state.done
+        })
+      )
+      .catch(error => console.log(error));
+  };
 
-
-
-	render() {
-		return (
-			<div className="todo-item">
-				<input
-					type="checkbox"
-					onClick={this.toggleDone}
-					defaultChecked={this.state.done}
-				/>
-				<p className={this.state.done ? 'done' : null}>{this.props.item.title}</p>
-				<button onClick={() => this.props.deleteItem(this.props.item.id)}>X</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="todo-item">
+        <input
+          type="checkbox"
+          onClick={this.toggleDone}
+          defaultChecked={this.state.done}
+        />
+        <p className={this.state.done ? "done" : null}>
+          {this.props.item.title}
+        </p>
+        <button onClick={() => this.props.deleteItem(this.props.item.id)}>
+          X
+        </button>
+      </div>
+    );
+  }
 }
 
 export default TodoItem;
